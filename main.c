63896a51ec188bab8489b3f8659095bbc0f6d4b2
TerryEmo <terrance.sabelo@gmail.com>
2023-09-20 11:47:21 -0700

    0x19. C - Stacks, Queues - LIFO, FIFO

--- main.c
+++ main.c
@@ -1,9 +1,9 @@
 #include "monty.h"
 bus_t bus = {NULL, NULL, NULL, 0};
 /**
-* main - monty code represented
-* @argc: num of arguments
-* @argv: monty file and location
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
 * Return: 0 on success
 */
 int main(int argc, char *argv[])
